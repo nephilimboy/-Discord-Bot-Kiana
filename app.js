31153b2a5446c767e69b750813d48e2a1d450439
amir <he.amirhossein@gmail.com>
2018-09-20 10:52:29 +0430

    fix port listening

--- app.js
+++ app.js
@@ -301,3 +301,9 @@ App.DynamicChannelName.loop = function (client, message) {
 
 App.run();
 
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+    console.log("Listening on Port 3000");
+});
